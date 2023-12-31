87b4e4b83f23ab85925dd754a68b038e63a0f475
soumyaranjan704 <57033716+soumyaranjan704@users.noreply.github.com>
2023-12-31 23:35:32 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'resume', component: ResumeComponent },
+  // { path: 'portfolio', component: PortfolioComponent },
+  // { path: 'services', component: ServicesComponent },
+  // { path: 'contact', component: ContactComponent },
+  // { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  // { path: '**', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home for unknown routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
